78e223d44f700eea70805017c35a6fcecd2a7c9d
juyeongnoh <juyeongnoh@gmail.com>
2024-02-04 18:21:46 +0900

    feat: 최단, 최장 기록 강조 효과

--- src/components/Laps.jsx
+++ src/components/Laps.jsx
@@ -1,16 +1,33 @@
 import formatTime from '../util/formatTime';
 
 const Laps = ({ laps }) => {
+  const lapTimeArr = laps.reduce((acc, cur) => [...acc, cur[1]], []);
+  const maxIdx = lapTimeArr.lastIndexOf(Math.max(...lapTimeArr));
+  const minIdx = lapTimeArr.lastIndexOf(Math.min(...lapTimeArr));
+
+  const minMaxStyle = (idx) => {
+    if (laps.length < 2) return;
+    if (idx === maxIdx) {
+      return 'text-red-600';
+    }
+    if (idx === minIdx) {
+      return 'text-green-600';
+    }
+    return '';
+  };
+
   return (
     <article className="h-64 overflow-auto text-gray-600 border-t-2">
       <ul>
-        {laps.map((lap) => (
+        {laps.map(([lapCount, lapTime], idx) => (
           <li
-            key={lap[0]}
-            className="flex justify-between px-3 py-2 border-b-2"
+            key={lapCount}
+            className={`flex justify-between px-3 py-2 border-b-2 ${minMaxStyle(
+              idx
+            )}`}
           >
-            <span>랩 {lap[0]}</span>
-            <span>{formatTime(lap[1])}</span>
+            <span>랩 {lapCount}</span>
+            <span>{formatTime(lapTime)}</span>
           </li>
         ))}
       </ul>
