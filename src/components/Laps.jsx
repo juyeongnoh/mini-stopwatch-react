cb5a5347b191b59abdd643f361c7cf53e3ef4cef
juyeongnoh <juyeongnoh@gmail.com>
2024-02-04 17:37:08 +0900

    feat: 랩 기능

--- src/components/Laps.jsx
+++ src/components/Laps.jsx
@@ -1,12 +1,15 @@
-const Laps = () => {
+import formatTime from '../util/formatTime';
+
+const Laps = ({ laps }) => {
   return (
     <article className="h-64 overflow-auto text-gray-600 border-t-2">
       <ul>
-        {/* 추가되는 lap은 아래의 HTML 코드 형식을 사용해 추가해주세요.  */}
-        {/* <li className="flex justify-between px-3 py-2 border-b-2">
-               <span>랩 5</span>
-               <span>00:00.28</span>
-           </li> */}
+        {laps.map((lap) => (
+          <li className="flex justify-between px-3 py-2 border-b-2">
+            <span>랩 {lap[0]}</span>
+            <span>{formatTime(lap[1])}</span>
+          </li>
+        ))}
       </ul>
     </article>
   );
